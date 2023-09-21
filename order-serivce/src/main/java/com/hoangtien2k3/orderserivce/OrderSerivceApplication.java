59eb0cc20681e0fdc575684a7cd7a6d1220d661c
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2023-09-21 10:58:23 +0700

    Update OrderSerivceApplication.java

--- order-serivce/src/main/java/com/hoangtien2k3/orderserivce/OrderSerivceApplication.java
+++ order-serivce/src/main/java/com/hoangtien2k3/orderserivce/OrderSerivceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableFeignClients
+@EnableWebSecurity
+@EnableRedisHttpSession
 public class OrderSerivceApplication {
 
 	public static void main(String[] args) {
