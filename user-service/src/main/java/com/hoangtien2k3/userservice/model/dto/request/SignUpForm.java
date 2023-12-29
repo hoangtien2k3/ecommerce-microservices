21be38a075482a2390128c40141b36d964caeec2
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-12-29 16:20:36 +0700

    api update

--- user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/request/SignUpForm.java
+++ user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/request/SignUpForm.java
@@ -12,48 +12,32 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class SignUpForm {
 
-//    @NotBlank
-//    @Size(min = 6, max = 50, message = "FullName must not be left blank")
-//    private String fullname;
-//
-//    @NotBlank
-//    @Size(min = 6, max = 50, message = "FullName must not be left blank")
-//    private String username;
-//
-//    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
-//    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)[a-zA-Z\\\\d]{8,}$",
-//            message = "Password must contain all uppercase and lowercase letters and numbers")
-//    private String password;
-//
-//    @Size(max = 50)
-//    @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", message = "Invalid email format")
-//    private String email;
-//
-//    @NotBlank(message = "Gender must not be left blank")
-//    private String gender;
-//
-//    @Pattern(regexp = "^\\+84[0-9]{9,10}$|^0[0-9]{9,10}$", message = "The phone number is not in the correct format")
-//    private String phone;
-//
-//    @Pattern(regexp = "^(http|https)://.*$", message = "Avatar URL must be a valid HTTP or HTTPS URL")
-//    private String avatar;
-//
-//    private Set<String> roles;
-
-
+    @NotBlank
+    @Size(min = 6, max = 50, message = "FullName must not be left blank")
     private String fullname;
+
+    @NotBlank
+    @Size(min = 6, max = 50, message = "FullName must not be left blank")
     private String username;
+
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)[a-zA-Z\\\\d]{8,}$",
+            message = "Password must contain all uppercase and lowercase letters and numbers")
     private String password;
+
+    @Size(max = 50)
+    @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Gender must not be left blank")
     private String gender;
+
+    @Pattern(regexp = "^\\+84[0-9]{9,10}$|^0[0-9]{9,10}$", message = "The phone number is not in the correct format")
     private String phone;
+
+    @Pattern(regexp = "^(http|https)://.*$", message = "Avatar URL must be a valid HTTP or HTTPS URL")
     private String avatar;
 
     private Set<String> roles;
-
-
-
-
-
 
 }
