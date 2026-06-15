f74f77069a7a593875d9a5859c427e7160157098
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2026-06-15 12:44:17 +0700

    feat: clean up YAML files by removing unnecessary blank lines

--- start-ecommerce.sh
+++ start-ecommerce.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-set -e
-
-docker compose up -d
-
-echo ""
-echo "Docker Deploy complete"
-echo "http://localhost:3000  -> Frontend"
-echo "http://localhost:8888  -> API Gateway"
-echo "http://localhost:8080  -> Keycloak"
-echo "http://localhost:9001  -> MinIO Console"
-echo "http://localhost:9411  -> Zipkin"
+chmod +x k3d-setup.sh
+./k3d-setup.sh
